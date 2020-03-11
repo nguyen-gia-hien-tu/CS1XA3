a894c316cce6a7887e2503057eb988b77a1930c3
Hien Tu <giahien196@gmail.com>
2020-03-11 00:24:00 -0400

    add files and feature, and modify README.md

--- Project02/README.md
+++ Project02/README.md
@@ -1,3 +1,22 @@
-Resume Template: https://www.w3schools.com/w3css/tryw3css_templates_cv.htm
+# CS1XA3 Project02 - tun1
 
-How to alter images in a loop: https://stackoverflow.com/questions/44546916/changing-images-with-javascript
+## Overview
+ This webpage is tun1's custom resume
+
+## Custom Javascript Code 
+
+* __Feature 1:__
+    * When you hover the mouse into one of the following words: __NGUYEN GIA HIEN TU__, __EDUCATION__, __EXTRACURRICULAR ACTIVITIES__; the background of the three words will change into a random (light) color
+    * Hover the mouse on one of the words again to "uncolor" the background color
+
+* __Feature 2:__
+    * Click on the words __NGUYEN GIA HIEN TU__, __SKILLS__, __AWARDS__, __EDUCATION__, __EXTRACURRICULAR ACTIVITIES__ to minimize/maximize the content of each corresponding section
+* __Feature 3:__
+    * The webpage automatically changes the image (avatar) after a few seconds
+
+## Reference:
+* The HTML document for the resume used the following HTML Template from w3schools: https://www.w3schools.com/w3css/tryw3css_templates_cv.htm
+
+* The snippets of Javascript code on how to generate random color is from: https://stackoverflow.com/questions/1484506/random-color-generator
+
+* The snippets of Javascript code on how to loop through images and display them one by one was altered from: https://stackoverflow.com/questions/10594925/loop-a-series-of-image-in-javascript
--- Project02/script.js
+++ Project02/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+
+    // Change "NGUYEN GIA HIEN TU", "EDUCATION", "ETRACURRICULAR ACTIVITIES" background color
     let numClick = 0
-    $(".bgCol").click(function(){
+    $(".bgCol").mouseenter(function(){
         if (numClick == 0){
-            $(".bgCol").css("backgroundColor", "cyan")
-                     .css("width", "75%");
+            $(".bgCol").css("backgroundColor", getRandomColor())
+                       .css("width", "75%");
             numClick = 1
         }
         else if (numClick == 1){
@@ -11,4 +13,48 @@ $(document).ready(function(){
             numClick = 0
         }
     });
+
+   function getRandomColor() {
+    var letters = '9ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 7)];
+        }
+    return color;
+    }
+
+    // Hide and Show "Skills" and "Awards"
+    $("#name").click(function(){
+        $("#info").toggle();
+    });
+
+    $("#skill").click(function(){
+        $("#skills").toggle();
+    });
+
+    $("#award").click(function(){
+        $("#awards").toggle();
+    });
+
+    $("#Edu").click(function(){
+        $("#Education").toggle();
+    });
+
+    $("#ExtraAct").click(function(){
+        $(".Activities").toggle();
+    });
+
+    // Change image
+    function imageLooping() {
+        let rotator = document.getElementById('image'),     //get the element
+            dir = 'Resume Images/',                         //images folder
+            num = 1,                                        //start number
+            len = 3;                                        //limit
+        setInterval(function(){                             //interval changer
+            rotator.src = dir + num +'.jpg';                //change picture
+            num = (num == len-1) ? 0 : ++num;               //reset if last image reached
+        }, 3000);
+    };
+    imageLooping();
 });
+
